dfc0b81817d571512628189d6a75ca3ce5fd1fab
unknown <bas.pennings@SPARE16.kantoor.tld>
2016-05-05 22:50:10 +0200

    Added some (mostly) infrastructural stuff

---
+++ config.example.ts
@@ -0,0 +1,9 @@
+const NICK = '';
+const PWD = '';
+const CHANNEL = '';
+
+export {
+NICK,
+PWD,
+CHANNEL
+}
--- index.ts
+++ index.ts
@@ -1,15 +1,52 @@
 /// <reference path="typings/main.d.ts" />
 
+import moment = require('moment');
 import levelup = require('levelup');
+import sublevel = require('level-sublevel');
+import irc = require('irc');
 
-const db = levelup('./db', {
+import {NICK, PWD, CHANNEL} from './config';
+
+var greet = require('greetings');
+
+const db = sublevel(levelup('./methbot.db', {
     valueEncoding: 'json'
+}));
+
+const people = db.sublevel('people');
+
+const client = new irc.Client('irc.freenode.net', NICK, {
+    channels: [CHANNEL]
 });
 
-db.put('foo', 'bar', (err) => {
-   if (err) return console.error(err);
-   db.get('foo', (err, val) => {
-      if (err) return console.error(err);
-      console.log(val); 
-   });
+client.on('pm', (from, text) => {
+    console.log(text);
+});
+
+client.on('message', (from, to, msg) => {
+    console.log(`${from} => ${to}: ${msg}`);
+});
+
+client.on('names', (channel, nicks) => {
+    console.log(nicks);
+});
+
+client.on('join', (channel, nick, msg) => {
+    var val = {
+       lastSeen: moment().toISOString(),
+       raw: msg 
+    };
+    
+    people.put(nick, val, (err) => {
+        if (err) console.error(err);
+    });
+});
+
+client.on('error', (msg) => {
+    console.error(msg);
+});
+
+client.join(`${CHANNEL} ${PWD}`, () => {
+    var msg = greet();
+    client.say(CHANNEL, msg);
 });
